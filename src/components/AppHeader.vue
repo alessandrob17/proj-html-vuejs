d0ef6feaf15307d20857aefe6d775ed4069e40a5
alessandrob17 <a.baldipardi@gmail.com>
2023-03-13 21:01:45 +0100

    FEAT push

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+   export default {
+    name: 'AppHeader'
+   }
+</script>
+
+<style scoped>
+.container {
+    background-color: grey;
+    height: 30px;
+}
+
+</style>
