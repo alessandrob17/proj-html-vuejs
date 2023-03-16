5ea9315faa99a10316e2060bdfedd5e53c08436b
alessandrob17 <a.baldipardi@gmail.com>
2023-03-16 01:17:13 +0100

    FEAT main ++

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -159,42 +159,226 @@
         </div>
     </section>
 
-    <!-- SECTION4 CARDS PERCENTAGE-->
+    <!-- SECTION4 CARDS PERCENTAGE ++-->
 
     <section>
         <div class="d-flex justify-content-center cards-percentage">
             <div class="card text-center" style="width: 18rem;">
                 <div class="card-body">
                     <h1 class="card-title pt-5 pb-3">95%</h1>
-                    
+
                     <p class="card-text pt-4 pb-4"><strong>PASS RATE</strong></p>
-                    
+
                 </div>
             </div>
 
             <div class="card text-center ms-3" style="width: 18rem;">
                 <div class="card-body">
                     <h1 class="card-title pt-5 pb-3">100%</h1>
-                    
+
                     <p class="card-text pt-4 pb-4"><strong>REFERRAL RATE</strong></p>
-                    
+
                 </div>
             </div>
 
             <div class="card text-center ms-3" style="width: 18rem;">
                 <div class="card-body">
                     <h1 class="card-title pt-5 pb-3">0%</h1>
-                    
+
                     <p class="card-text pt-4 pb-4"><strong>ACCIDENT RATE</strong></p>
-                    
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="pt-5 justify-content-center d-flex">
+            <div>
+                <h3 class="py-2">
+                    Your Instructors
+                </h3>
+
+                <div class="d-flex align-items-center">
+                    <p class="me-5 pt-2" style="width: 33rem;">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Distinctio provident quam pariatur. Corporis, illum aliquam.</p>
+                    <button type="button" class="btn btn-success ms-5 px-5">Success</button>
+                </div>
+
+
+            </div>
+
+
+        </div>
+        <div class="d-flex justify-content-center pt-5 mt-3 py-5">
+            <div class="col-2 pe-2 ">
+                <div class="card">
+                    <img src="../assets/img/instructor-mikehart-600x381.jpg" class="card-img-top px-4 py-4" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mike Heart</h5>
+                        <div>
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-twitter px-3"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-2 pe-2">
+                <div class="card">
+                    <img src="../assets/img/instructor-angelahart-200x127.jpg" class="card-img-top px-4 py-4" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Angela Heart</h5>
+                        <div>
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-twitter px-3"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-2 pe-2">
+                <div class="card">
+                    <img src="../assets/img/instructor-johnsmith-200x127.jpg" class="card-img-top px-4 py-4" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">John Smith</h5>
+                        <div>
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-twitter px-3"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. </p>
+                    </div>
                 </div>
             </div>
 
 
 
         </div>
+
+    </section>
+
+    <!-- SECTION5 -->
+
+    <section>
+        <div class="bg-road-tree">
+            <div class="text-center py-5">
+                <h2 class="pt-5 py-3 mt-5">
+                    Testimonials
+                </h2>
+
+                <p class="pb-5">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, architecto.
+                </p>
+
+                <figure class="pt-2 pb-4">
+                    <img style="width: 9rem;" src="../assets/img/testimonial-sophia.png" alt="">
+                </figure>
+
+                <p class="pt-4 pb-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aut <br> accusamus aspernatur maxime
+                    quisquam reiciendis qui dolor quas? <br> Hic, mollitia.
+                </p>
+
+                <h4 class="pt-2">
+                    Sophia Jones
+                </h4>
+
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- SECTION6 -->
+
+    <section>
+        <div class="s6-bg d-flex justify-content-center">
+            <div class="bg-white-s6  ">
+                <h2 class="text-center pt-3">
+                    Latest news
+                </h2>
+                <div class="d-flex justify-content-center pt-4">
+
+                    <div class="pe-4">
+                        <figure>
+                            <img src="../assets/img/blog-choosecar-320x202.jpg" alt="">
+                        </figure>
+                        <div class="text-center">
+                            <h4 class="pt-4">
+                                What car to start with
+                            </h4>
+                            <p>
+                                February 7th 2019 | <span>0 comments</span>
+                            </p>
+                            <p class="pt-2">
+                                Lorem ipsum dolor sit amet consectetur.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <figure>
+                            <img src="../assets/img/blogpost-10and2-320x202.jpg" alt="">
+                        </figure>
+                        <div class="text-center">
+                            <h4 class="pt-4">
+                                What car to start with
+                            </h4>
+                            <p>
+                                February 7th 2019 | <span>0 comments</span>
+                            </p>
+                            <p class="pt-2">
+                                Lorem ipsum dolor sit amet consectetur.
+                            </p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="text-center pt-3 ">
+                    <button type="button" class="btn btn-success px-5">More from the blog</button>
+                </div>
+
+                <figure>
+                    <img class="triangolo-white" src="../assets/img/latestnews-bottom-1200x105.png" alt="">
+                </figure>
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- SECTION7 LETTER -->
+
+    <section>
+        <div class="text-center pt-4">
+            <h3>
+                Sign up for Newsletter
+            </h3>
+            <div class="d-flex justify-content-center pt-5">
+                <input style="width: 30rem;" type="email" class="form-control " id="exampleFormControlInput1"
+                    placeholder="name@example.com">
+
+            </div>
+            <div class="pt-3">
+                <button style="width: 25rem;" type="button" class="btn btn-success px-5">Subscribe</button>
+            </div>
+            <p class="pt-4">
+                (We do not share your data with anybody, and nly use it for its intended purpose)
+            </p>
+        </div>
+
     </section>
 </template>
+
+
 
 <script>
 export default {
@@ -202,7 +386,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" >
 * {
     margin: 0;
     padding: 0;
@@ -268,7 +452,27 @@ ul {
     margin: -25px;
 }
 
+.bg-road-tree {
+    background-image: url('../assets/img/testimonial-background.jpg');
+    background-size: cover;
+    height: 90vh;
+}
 
+.s6-bg {
+    background-image: url('../assets/img/latestnews-background.jpg');
+    background-size: cover;
+    height: 95vh;
+}
 
+.bg-white-s6 {
+    background-color: white;
+    width: 750px;
+    height: 550px;
+    margin: -19px;
+    border-radius: 10px;
+}
 
+.triangolo-white {
+    margin-top: 40px;
+}
 </style>
